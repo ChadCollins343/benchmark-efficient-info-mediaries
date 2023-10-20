15805dc03a8536cf610238778ea3566a9c63373d
Chad Collins <144498558+ChadCollins343@users.noreply.github.com>
2023-10-20 17:56:38 +0300

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,30 @@
+    main()
+def generate_random_data():
+    return data
+    for item in data:
+import random
+
+
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+main();
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+function main() {
+}
+
+    const data = [];
+    });
+}
+    const data = generateRandomData();
+
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+
+    return data;
+function generateRandomData() {
