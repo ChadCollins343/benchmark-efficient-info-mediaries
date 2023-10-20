112ac2452268722da7423284465aaa4e92a1fd26
Chad Collins <144498558+ChadCollins343@users.noreply.github.com>
2023-10-21 02:57:21 +0300

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,53 @@
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+def generate_random_data():
+
+
+import random
+    return data
+def main():
+    data = generate_random_data()
+
+
+    for item in data:    data = [random.randint(1, 100) for _ in range(10)]
+
+def generate_random_data():
+    main()
+    data = generate_random_data()
+
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+
+    for item in data:
+import random
+
+    return data
+def main():
+
+
+
+    for item in data:
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+if __name__ == "__main__":
+    main()
+def main():
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+    return datadef main():
+        print(f"Random Number: {item}")
+    main()
+
+
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def generate_random_data():
+    return data
+import random
+
+if __name__ == "__main__":
+    data = generate_random_data()
